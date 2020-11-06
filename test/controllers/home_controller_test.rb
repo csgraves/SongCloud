c84e7cdf4ea2d49e612c7cb8e6d6982cbd03ea7e
Conor Graves <cg00853@surrey.ac.uk>
2020-11-06 12:03:50 -0800

    Added controller testing

--- test/controllers/home_controller_test.rb
+++ test/controllers/home_controller_test.rb
@@ -2,8 +2,15 @@ require 'test_helper'
 
 class HomeControllerTest < ActionDispatch::IntegrationTest
   test "should get home" do
-    get home_home_url
+    get root_url
+    assert_select 'title', 'Coursework App'
+    assert_select 'h1', 'My Coursework'
+    assert_select 'p', 'Welcome to my coursework site!'
+
+
     assert_response :success
   end
+
+
 
 end
